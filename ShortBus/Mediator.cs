2227e39152c8fdeacf95b9b530b626feb75777ca
Martin Gemme <martin.gemme@equisoft.com>
2020-12-01 11:26:58 -0500

    4.1 Changes

--- ShortBus/Mediator.cs
+++ ShortBus/Mediator.cs
@@ -44,17 +44,30 @@ namespace ShortBus
 
         public async Task<Response<TResponseData>> RequestAsync<TResponseData>(IAsyncRequest<TResponseData> query)
         {
-            var response = new Response<TResponseData>();
-
+            Response<TResponseData> response = new Response<TResponseData>();
             try
             {
-                var plan = new MediatorPlan<TResponseData>(typeof (IAsyncRequestHandler<,>), "HandleAsync", query.GetType(), _dependencyResolver);
+                var plan = new MediatorPlan<TResponseData>(typeof(IAsyncRequestHandler<,>), "HandleAsync", query.GetType(), this._dependencyResolver);
+                var implementationMethod = plan.HandlerInstance.GetType().GetMethod(plan.HandleMethod.Name, ((IEnumerable<ParameterInfo>)plan.HandleMethod.GetParameters()).Select(info => info.ParameterType).ToArray());
+                var interceptors = implementationMethod.GetCustomAttributes()
+                    .Where(attribute => attribute is RequestInterceptAttribute)
+                    .Cast<RequestInterceptAttribute>()
+                    .SelectMany(attribute => attribute.GetInterceptors())
+                    .Select(type => _dependencyResolver.GetInstance(type))
+                    .Cast<RequestInterceptor>()
+                    .ToList();
+                
+                foreach (RequestInterceptor requestInterceptor in interceptors)
+                    requestInterceptor.BeforeInvoke(plan.HandleMethod, query, query.GetType());
 
                 response.Data = await plan.InvokeAsync(query);
+
+                foreach (RequestInterceptor requestInterceptor in interceptors)
+                    requestInterceptor.AfterInvoke(plan.HandleMethod, query, query.GetType(), response.Data);
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                response.Exception = e;
+                response.Exception = ex;
             }
 
             return response;
@@ -62,22 +75,22 @@ namespace ShortBus
 
         public Response Notify<TNotification>(TNotification notification)
         {
-            var handlers = _dependencyResolver.GetInstances<INotificationHandler<TNotification>>();
-
-            var response = new Response();
-            List<Exception> exceptions = null;
-
-            foreach (var handler in handlers)
+            IEnumerable<INotificationHandler<TNotification>> instances = this._dependencyResolver.GetInstances<INotificationHandler<TNotification>>();
+            Response response = new Response();
+            List<Exception> exceptionList = null;
+            foreach (INotificationHandler<TNotification> notificationHandler in instances)
+            {
                 try
                 {
-                    handler.Handle(notification);
+                    notificationHandler.Handle(notification);
                 }
-                catch (Exception e)
+                catch (Exception ex)
                 {
-                    ( exceptions ?? ( exceptions = new List<Exception>() ) ).Add(e);
+                    (exceptionList ?? (exceptionList = new List<Exception>())).Add(ex);
                 }
-            if (exceptions != null)
-                response.Exception = new AggregateException(exceptions);
+            }
+            if (exceptionList != null)
+                response.Exception = new AggregateException(exceptionList);
             return response;
         }
 
@@ -107,9 +120,9 @@ namespace ShortBus
             {
                 await asyncCommandHandler.HandleAsync(message);
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                return e;
+                return ex;
             }
 
             return null;
@@ -117,16 +130,16 @@ namespace ShortBus
 
         class MediatorPlan<TResult>
         {
-            readonly MethodInfo HandleMethod;
-            readonly Func<object> HandlerInstanceBuilder;
+            public MethodInfo HandleMethod;
+            public Func<object> HandlerInstanceBuilder;
+            public object HandlerInstance;
 
             public MediatorPlan(Type handlerTypeTemplate, string handlerMethodName, Type messageType, IDependencyResolver dependencyResolver)
             {
-                var handlerType = handlerTypeTemplate.MakeGenericType(messageType, typeof (TResult));
-
+                var handlerType = handlerTypeTemplate.MakeGenericType(messageType, typeof(TResult));
                 HandleMethod = GetHandlerMethod(handlerType, handlerMethodName, messageType);
-
                 HandlerInstanceBuilder = () => dependencyResolver.GetInstance(handlerType);
+                HandlerInstance = HandlerInstanceBuilder();
             }
 
             MethodInfo GetHandlerMethod(Type handlerType, string handlerMethodName, Type messageType)
@@ -141,12 +154,12 @@ namespace ShortBus
 
             public TResult Invoke(object message)
             {
-                return (TResult) HandleMethod.Invoke(HandlerInstanceBuilder(), new[] { message });
+                return (TResult) HandleMethod.Invoke(HandlerInstance, new[] { message });
             }
 
             public async Task<TResult> InvokeAsync(object message)
             {
-                return await (Task<TResult>) HandleMethod.Invoke(HandlerInstanceBuilder(), new[] { message });
+                return await (Task<TResult>) HandleMethod.Invoke(HandlerInstance, new[] { message });
             }
         }
     }
